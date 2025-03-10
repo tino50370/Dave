a20348016058fe29e4050d66bb4f3c0151cd0a37
SepiaBeet50370 <tinoda.matewe@students.uz.ac.zw>
2025-03-10 13:37:02 +0200

    test4

--- AWSLambdaFunctions/read_Files/lambda_function.py
+++ AWSLambdaFunctions/read_Files/lambda_function.py
@@ -3,7 +3,7 @@ import urllib.request
 import os
 import base64
 
-#testing1234
+#testing12345
 # GitHub repository details
 GITHUB_OWNER = "tino50370"  # Replace with your GitHub username/org
 GITHUB_REPO = "Django-MVC"  # Replace with your repo name
